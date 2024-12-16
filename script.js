74e1875bc31f9e4eb20fd371f1b99f43329bfd2c
Byaombe <34215129+byaombe@users.noreply.github.com>
2024-12-16 04:38:52 -0600

    created new book card dynamically, added option to crea the card, and close after submitting book info, and added a check box

--- script.js
+++ script.js
@@ -47,16 +47,46 @@ this.info = () =>{
      this.author = theAuthor;
      this.pages = pageNumbers;
 
-    //display values
-    const p1 = document.getElementById("book-title");
-    const p2 = document.getElementById("book-author");
-    const p3 = document.getElementById("book-pages");
 
-    p1.textContent = "Title: " + this.title;
-    p2.textContent = "Author: " + this.author;
-    p3.textContent = "Pages: " + this.pages;
+     // Create the new book card dynamically
+     const newBookCard = document.createElement('div');
+     newBookCard.classList.add('book-card');
 
+     // Create the  card section with title, author, pages
+     const card1 = document.createElement('div');
+     card1.classList.add('card');
+     card1.innerHTML = `
+       <p id="book-title">Book Title: ${theTitle}</p>
+       <p id="book-author">The Author: ${theAuthor}</p>
+       <p id="book-pages">Pages: ${pageNumbers}</p>
+      
+     `;
+const toggleSWitch = document.createElement('div');
+toggleSWitch.classList.add('toggle');
+toggleSWitch.innerHTML = `
+      <label for="toggle" class="toggle-switch">
+      <input type="checkbox" id="toggle">
+      <span class="toggle-slider"></span>
+      </label>
+      <span id="status">Read the book</span>
+`;
+
+newBookCard.appendChild(card1);
+card1.appendChild(toggleSWitch);
+
+
+
+      // Append the new book card to the main book-card
+      document.querySelector('.book-card').appendChild(newBookCard);
   
+
+      //clear the input field
+      document.getElementById("title").value = "";
+      document.getElementById("author").value = "";
+      document.getElementById("pages").value = "";
+
+      // Close the popup 
+      popupForm.style.display = 'none';
   })
 }
 
