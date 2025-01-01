86bbdc640ddf39bb905555c48aacf9348bcccc0b
Byaombe <34215129+byaombe@users.noreply.github.com>
2025-01-01 07:59:07 -0600

    refactored js to use classes instead of constructors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+"use strict";
 //add our DOM
 const openForm = document.getElementById("openForm");
 const popupForm = document.getElementById("container-popup");
@@ -21,10 +21,87 @@ popupForm.style.display = 'none';
 
 const myLibrary = [];
 
+//the popup
+class Book {
+  constructor(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+  } //end of constructor
+  info = () =>{
+
+    const addBtn = document.getElementById("add-btn");
+  
+    //save info when clicked 
+    addBtn.addEventListener('click', () => {
+    
+      //get title, author and pages
+      const theTitle = document.getElementById("title").value;
+      const theAuthor = document.getElementById("author").value;
+      const pageNumbers = document.getElementById("pages").value;
+  
+      //create a newBook instance with the entered values
+      const newBook = new Book (theAuthor, theTitle, pageNumbers);
+      //Add the book objects to the library array
+      myLibrary.push(newBook);
+
+       // Create the new book card dynamically
+       const newBookCard = document.createElement('div');
+       newBookCard.classList.add('book-card');
+  
+       // Create the  card section with title, author, pages
+       const card1 = document.createElement('div');
+       card1.classList.add('card');
+       card1.innerHTML = `
+         <p id="book-title">Book Title: ${theTitle}</p>
+         <p id="book-author">The Author: ${theAuthor}</p>
+         <p id="book-pages">Pages: ${pageNumbers}</p>
+        
+       `;
+  const toggleSWitch = document.createElement('div');
+  toggleSWitch.classList.add('toggle');
+  toggleSWitch.innerHTML = `
+        <label for="toggle" class="toggle-switch">
+        <input type="checkbox" id="toggle">
+        <span class="toggle-slider"></span>
+        </label>
+        <span id="status">Read the book</span>
+  `;
+  
+  newBookCard.appendChild(card1);
+  card1.appendChild(toggleSWitch);
+  
+  
+  
+        // Append the new book card to the main book-card
+        document.querySelector('.book-card').appendChild(newBookCard);
+    
+  
+        //clear the input field
+        document.getElementById("title").value = "";
+        document.getElementById("author").value = "";
+        document.getElementById("pages").value = "";
+  
+        // Close the popup 
+        popupForm.style.display = 'none';
+    })
+  }
+}
+const myBook = new Book();
+myBook.info();
+
+
+
+//push book to the library  array
+function addBookToLibrary() {
+myLibrary.push(Book);
+
+}
+
 
 //DOM FOR THE POPUP
 
-
+/*
 function Book(author, title, pages) {
   // the constructor...
 this.author = author;
@@ -100,3 +177,4 @@ myBook.info();
 function addBookToLibrary() {
   // do stuff here
 }
+*/
